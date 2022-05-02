2567d286bcaa9bd8bd29625935c63f5a4595ceb6
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2022-05-02 12:47:17 -0300

    Novo validador

--- lib/src/input_validators/input_validators.dart
+++ lib/src/input_validators/input_validators.dart
@@ -1,3 +1,6 @@
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+
 bool isNotEmptyValidator(String value) {
   return value.isNotEmpty;
 }
@@ -24,6 +27,17 @@ bool isGreaterThenValue({
 bool isMatchValidator({required String value, required String otherValue}) {
   if (value != otherValue) return false;
   return true;
+}
+
+bool isAValidDate(String value) {
+  initializeDateFormatting();
+  final DateFormat formatter = DateFormat('dd/MM/yyyy');
+  try {
+    var parsedDate = DateFormat('dd/MM/yyyy', 'pt_BR').parse(value);
+    return formatter.format(parsedDate) == value;
+  } on Exception catch (_) {
+    return false;
+  }
 }
 
 convertCommaToPointValue({required String value}) {
