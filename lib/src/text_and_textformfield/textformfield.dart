5ff71aa8fef059076dab4ecca214c6d1dabe9905
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2023-05-11 14:16:44 -0300

    Update textformfield.dart

--- lib/src/text_and_textformfield/textformfield.dart
+++ lib/src/text_and_textformfield/textformfield.dart
@@ -222,8 +222,8 @@ class _TextFormFieldECAState extends State<TextFormFieldECA> {
         filled: true,
         errorMaxLines: widget.errorMaxLines,
         contentPadding: EdgeInsets.only(
-          // top: 10.0,
-          top: widget.showLabelText ? 30 : 10.0,
+          top: 10.0,
+          // top: widget.showLabelText ? 30 : 10.0,
           bottom: widget.bottomContentPadding ?? 10.0,
           // bottom:
           //     widget.showLabelText ? 30 : widget.bottomContentPadding ?? 10.0,
